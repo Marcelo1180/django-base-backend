0e3864da8f9e36fdbbee62f5b45b3bdfa9b647f2
Marcelo Arteaga <arteagamarcelo@gmail.com>
2021-10-31 01:35:58 -0400

    adding support to static files

--- base/urls.py
+++ base/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -53,4 +54,4 @@ if settings.DEBUG:
             schema_view.with_ui("swagger", cache_timeout=0),
             name="schema-swagger-ui",
         ),
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
