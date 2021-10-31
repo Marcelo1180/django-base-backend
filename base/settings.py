838ab05e9141055a15fa52053ed7559f3d0bd786
Marcelo Arteaga <arteagamarcelo@gmail.com>
2021-10-31 07:36:57 -0400

    adding cors support

--- base/settings.py
+++ base/settings.py
@@ -147,11 +147,16 @@ SWAGGER_SETTINGS = {
     },
 }
 
+# Cors headers
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_HEADERS = (
-    "x-requested-with",
-    "content-type",
+CORS_ALLOW_HEADERS = [
     "accept",
-    "origin",
+    "accept-encoding",
     "authorization",
-)
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
