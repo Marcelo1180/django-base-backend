bbd2187a5320cd2b490c3b2408f1ff7e6a2df7bb
Marcelo Arteaga <arteagamarcelo@gmail.com>
2022-03-23 19:15:24 -0400

    Updating url path new changes

--- base/urls.py
+++ base/urls.py
@@ -1,21 +1,5 @@
-"""base URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls import url, include
+from django.urls import path, include, re_path
 from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -48,12 +32,12 @@ schema_view = get_schema_view(
 if settings.DEBUG:
     urlpatterns += [
         path("admin/", admin.site.urls),
-        url(
+        re_path(
             r"^apidoc(?P<format>\.json|\.yaml)$",
             schema_view.without_ui(cache_timeout=0),
             name="schema-json",
         ),
-        url(
+        re_path(
             r"^apidoc/$",
             schema_view.with_ui("swagger", cache_timeout=0),
             name="schema-swagger-ui",
