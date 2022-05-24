71fa2c101b714ac1b8658040afc8d585f82c418c
Marcelo1180 <arteagamarcelo@gmail.com>
2022-05-24 14:20:30 -0400

    Fix debug env var

--- base/settings.py
+++ base/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "change-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = eval(os.environ.get("DEBUG", "False"))
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "")]
 
 MIDDLEWARE = [
